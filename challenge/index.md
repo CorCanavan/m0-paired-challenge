95d5412440c5f169eadea120484254943873a3a4
“Corinne <corinne.canavan@gmail.com>
2022-02-09 20:28:31 -0700

    Complete Mod 0 Paired Challenge

---
+++ challenge/index.md
@@ -0,0 +1,8 @@
+# H1 Book Titles
+1. Girl on the Train
+2. Gone Girl
+3. Into the Water
+4. Atomic Habits
+5. Big Magic
+
+Cyanne and I drove on our own computers while collaborating. I noticed I should have read through the instructions and steps more carefully so I could better name the .js file. I also realized how hard it is to come up with accurate, descriptive variable names on the fly. 
---
+++ challenge/js_practice.js
@@ -0,0 +1,13 @@
+//I chose js_practice since we are practicing javascript but I realize now I should have been more specific to the content within this file and called it book_challenge.js
+var favoriteFantasyBook = "Harry Potter and the Goblet of Fire";
+var selfHelpBook = "Atomic Habits";
+var readBook = true;
+//this variable is a Boolean depending on whether the book has been read or not.
+var numberOfBooks = 52;
+var recentlyReadBook = "Big Little Lies";
+//this variables contains a string data type referring to my most recently read book.
+var currentBook = "Good Vibes, Good Life";
+//this variable contains a string data type referring to the book I am currently reading.
+
+var HarryPotterBookTitles = ["Sorcerers Stone", "Chamber of Secrets", "Prisoner of Azkaban", "Goblet of Fire", "Order of the Phoenix", "Halfblood Prince", "Deathly Hallows"];
+//all the books are part of the Harry Potter series, so I picked HarryPotterBookTitles as the array name.
